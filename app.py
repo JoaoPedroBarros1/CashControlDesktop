d46241fb08d1f979eef5cfd2dab85e063a6e0b49
Aluno <jpe.bgi@gmail.com>
2024-04-08 17:27:25 -0300

    Changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_bcrypt import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = 'chave_boa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/cashcontrol'
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
 
 # ---------------------- CLASSES ------------------------
@@ -16,24 +16,21 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    telefone = db.Column(db.String(20))
-    rg = db.Column(db.String(20))
-    cpf = db.Column(db.String(14))
-    banco = db.Column(db.String(50))
-    agencia = db.Column(db.String(10))
-    conta = db.Column(db.String(20))
     senha = db.Column(db.String(30))
 
 
 class Receita(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # nome
-    # data_emissao
-    # valor_receitas
+    id_receita = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(255))
+    data_emissao = db.Column(db.Date)
+    valor_receita = db.Column(db.Numeric(10, 2))
 
 
 class Despesa(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_despesa = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(255))
+    data_emissao = db.Column(db.Date)
+    valor_despesa = db.Column(db.Numeric(10, 2))
 
 
 # ------------------- ROTAS PRINCIPAIS -------------------
@@ -41,13 +38,121 @@ class Despesa(db.Model):
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    if 'id' in session:
+        return render_template(url_for('dashboard'))
+    else:
+        return render_template('home.html')
+
+
+@app.route('/login')
+def login_form():
+    if 'id' in session:
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/cadastro')
+def cadastro_form():
+    if 'id' in session:
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('cadastro.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'id' in session:
+        return render_template('dashboard.html', email=session['email'])
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/receitas')
+def receitas():
+    if 'id' in session:
+        return render_template('receitas.html')
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/despesas')
+def despesas():
+    if 'id' in session:
+        return render_template('despesas.html')
+    else:
+        return redirect(url_for('index'))
 
 
 # ------------------- ROTAS INTERMEDIARIAS -----------------
 
 
-# a
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = Usuario.query.filter_by(email=email).first()
+    if not user:
+        return redirect(url_for('login_form'))
+
+    senha = check_password_hash(user.senha, password)
+    if not senha:
+        return redirect(url_for('login_form'))
+
+    session['id'] = user.id
+    return redirect(url_for('dashboard'))
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('id', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/cadastro', methods=['POST'])
+def cadastro_post():
+    username = request.form['username']
+    email = request.form['email']
+    senha = request.form['password']
+
+    user = Usuario.query.filter_by(email=email).first()
+    if not user:
+        senha_hash = generate_password_hash(senha).decode('utf-8')
+        new_user = Usuario(
+            nome=username, email=email,
+            senha=senha_hash)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return redirect(url_for('cadastro_form'))
+
+
+@app.route('/adicionar_receita', methods=['POST'])
+def adicionar_receita():
+    nome = request.form['nome']
+    data = request.form['data']
+    preco = request.form['preco']
+
+    nova_receita = Receita(nome=nome, data_emissao=data, valor_receitas=preco)
+    db.session.add(nova_receita)
+    db.session.commit()
+
+    return redirect(url_for('dashboard'))
+
+
+@app.route('/adicionar_despesa', methods=['POST'])
+def adicionar_despesa():
+    nome = request.form['nome']
+    data = request.form['data']
+    preco = request.form['preco']
+
+    nova_despesa = Despesa(nome=nome, data_emissao=data, valor_despesas=preco)
+    db.session.add(nova_despesa)
+    db.session.commit()
+
+    return redirect(url_for('dashboard'))
 
 
 # ----------------------- APP ---------------------
