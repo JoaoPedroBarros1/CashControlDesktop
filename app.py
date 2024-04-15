67668f7bd7d0f776c6f1872dfdf01d7a3d2700cd
Aluno <jpe.bgi@gmail.com>
2024-04-15 09:40:18 -0300

    Not working: Dashboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory, session
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import generate_password_hash, check_password_hash
 
@@ -16,11 +16,12 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    senha = db.Column(db.String(30))
+    senha = db.Column(db.String(100))
 
 
 class Receita(db.Model):
     id_receita = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_usuario = db.Column(db.Integer, nullable=False)
     nome = db.Column(db.String(255))
     data_emissao = db.Column(db.Date)
     valor_receita = db.Column(db.Numeric(10, 2))
@@ -28,6 +29,7 @@ class Receita(db.Model):
 
 class Despesa(db.Model):
     id_despesa = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_usuario = db.Column(db.Integer, nullable=False)
     nome = db.Column(db.String(255))
     data_emissao = db.Column(db.Date)
     valor_despesa = db.Column(db.Numeric(10, 2))
@@ -39,7 +41,7 @@ class Despesa(db.Model):
 @app.route('/')
 def index():
     if 'id' in session:
-        return render_template(url_for('dashboard'))
+        return redirect(url_for('dashboard'))
     else:
         return render_template('home.html')
 
@@ -63,7 +65,26 @@ def cadastro_form():
 @app.route('/dashboard')
 def dashboard():
     if 'id' in session:
-        return render_template('dashboard.html', email=session['email'])
+        list_receitas = Receita.query.filter_by(id_usuario=session['id']).all()
+        list_despesas = Despesa.query.filter_by(id_usuario=session['id']).all()
+
+        total_receitas = 0.00
+        total_despesas = 0.00
+
+        for receita in list_receitas:
+            total_receitas += float(receita.valor_receita)
+
+        for despesa in list_despesas:
+            total_despesas += float(despesa.valor_despesa)
+
+        total_saldo = total_receitas - total_despesas
+
+        return render_template('dashboard.html',
+                               receitas=list_receitas,
+                               despesas=list_despesas,
+                               total_saldo=total_saldo,
+                               total_receitas=total_receitas,
+                               total_despesas=total_despesas)
     else:
         return redirect(url_for('index'))
 
@@ -98,6 +119,7 @@ def login_post():
 
     senha = check_password_hash(user.senha, password)
     if not senha:
+        print("No senha:", user.senha, generate_password_hash(password))
         return redirect(url_for('login_form'))
 
     session['id'] = user.id
@@ -135,7 +157,10 @@ def adicionar_receita():
     data = request.form['data']
     preco = request.form['preco']
 
-    nova_receita = Receita(nome=nome, data_emissao=data, valor_receitas=preco)
+    if float(preco) <= 0:
+        return redirect(url_for("receitas"))
+
+    nova_receita = Receita(id_usuario=session['id'], nome=nome, data_emissao=data, valor_receita=preco)
     db.session.add(nova_receita)
     db.session.commit()
 
@@ -148,7 +173,10 @@ def adicionar_despesa():
     data = request.form['data']
     preco = request.form['preco']
 
-    nova_despesa = Despesa(nome=nome, data_emissao=data, valor_despesas=preco)
+    if float(preco) <= 0:
+        return redirect(url_for("despesas"))
+
+    nova_despesa = Despesa(id_usuario=session['id'], nome=nome, data_emissao=data, valor_despesa=preco)
     db.session.add(nova_despesa)
     db.session.commit()
 
